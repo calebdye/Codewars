7c4b8831cf303f0e7e43ecce366504a2061dcb7c
caleb <calebemorydye@gmail.com>
2022-11-14 11:17:11 -0700

    CW 7 strings

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log('hello')
+function sortByLength (array) {
+    // Return an array containing the same strings, ordered from shortest to longest
+    array.sort((a, b) => a.length - b.length)
+    return array
+  };
