fe12dc2dc2f5bfe58e348e62742109f1b163b4c5
caleb <calebemorydye@gmail.com>
2022-11-25 17:59:40 -0700

    simple functions

---
+++ simple functions.js
@@ -0,0 +1,23 @@
+//---Easy
+//create a function that subtracts two numbers and alerts the difference
+function sub(a,b) {
+    alert(a-b)
+}
+sub(5,7)
+//create a function that divides three numbers and console logs the quotient
+function div(c,d,e) {
+    console.log(c/d/e)
+}
+div(5,7,8)
+//create a function that multiplys three numbers and returns the product
+function mul(f,g,h) {
+    return f*g*h
+}
+mul(5,7,8)
+//---Medium
+//create a function that takes in three numbers. Add the first two numbers 
+// and return the remainder of dividing the sum of the first two numbers by the third number
+function wei(l,k,m) {
+    console.log((l+k)%m)
+}
+wei(5,7,8)
