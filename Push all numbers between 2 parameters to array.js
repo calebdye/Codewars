60f249814a5281540ee365f4a51f6f3faa6b1b21
caleb <calebemorydye@gmail.com>
2022-11-30 12:14:21 -0700

    Push all nums between 2 par to array

---
+++ Push all numbers between 2 parameters to array.js
@@ -0,0 +1,15 @@
+// function between(a, b) {
+//   let  arr =[]
+//   for(let i = a; i<=b; i++){
+//     arr.push(i)
+//   } return arr
+// }
+
+
+
+let between = (a,b) => {
+    let  arr =[]
+  for(let i = a; i<=b; i++){
+    arr.push(i)
+  } return arr
+};
